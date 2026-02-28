2349f2a919f06f66096f7d4df98eff1252b03048
Orisunmibare Muiz Olamilekan <orismuiz2003@gmail.com>
2026-02-28 19:41:58 +0100

    feat: create Home.jsx

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HeroSection from '../Components/HeroSection'
+import Nav from '../Components/Nav'
+import About from '../Components/About'
+
+export default function Home() {
+    return (
+        <>
+           <Nav />
+            <HeroSection />
+            <About />
+        </>
+    )
+}
